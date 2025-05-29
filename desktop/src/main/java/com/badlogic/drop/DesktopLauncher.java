b418d1da3f808cae2595f0a72a6efb67a0cdfefc
Made9Ts <165938562+Made9Ts@users.noreply.github.com>
2025-05-29 22:56:40 +0300

    2.3

---
+++ desktop/src/main/java/com/badlogic/drop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+/**
+ * Запускает игру на компьютере
+ */
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("Space Courier");
+        config.setWindowedMode(720, 1080);
+        new Lwjgl3Application(new SpaceCourierGame(), config);
+    }
+} 
